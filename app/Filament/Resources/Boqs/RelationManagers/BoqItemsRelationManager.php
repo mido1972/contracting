80d7c9054518a46f498784a520a4093503ea4e5a
SHERIF\SHERIF <sherif.ganainy@gmail.com>
2025-12-21 23:46:32 +0200

    Add BoqItems RelationManager scaffold

---
+++ app/Filament/Resources/Boqs/RelationManagers/BoqItemsRelationManager.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\Boqs\RelationManagers;
+
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Table;
+
+class BoqItemsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'items';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                \Filament\Tables\Columns\TextColumn::make('id')
+                    ->label('#'),
+            ]);
+    }
+}
