efc9fca7f4bcdedc0558d1951a84491ca41d09d2
SHERIF\SHERIF <sherif.ganainy@gmail.com>
2025-12-22 00:03:26 +0200

    Boq items RelationManager: create/edit/delete minimal form

--- app/Filament/Resources/Boqs/RelationManagers/BoqItemsRelationManager.php
+++ app/Filament/Resources/Boqs/RelationManagers/BoqItemsRelationManager.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Boqs\RelationManagers;
 
 use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
+use Filament\Forms;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables\Table;
 
@@ -15,9 +18,82 @@ class BoqItemsRelationManager extends RelationManager
         return $table
             ->columns([
                 \Filament\Tables\Columns\TextColumn::make('id')->label('#'),
+
+                \Filament\Tables\Columns\TextColumn::make('workItem.name')
+                    ->label('بند العمل')
+                    ->wrap(),
+
+                \Filament\Tables\Columns\TextColumn::make('unit.name')
+                    ->label('الوحدة'),
+
+                \Filament\Tables\Columns\TextColumn::make('quantity')
+                    ->label('الكمية'),
+
+                \Filament\Tables\Columns\TextColumn::make('unit_price')
+                    ->label('سعر الوحدة'),
+
+                \Filament\Tables\Columns\TextColumn::make('total_price')
+                    ->label('الإجمالي'),
             ])
             ->headerActions([
-                CreateAction::make(),
+                CreateAction::make()
+                    ->form([
+                        Forms\Components\Select::make('work_item_id')
+                            ->label('بند العمل')
+                            ->relationship('workItem', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Forms\Components\Select::make('unit_id')
+                            ->label('الوحدة')
+                            ->relationship('unit', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('quantity')
+                            ->label('الكمية')
+                            ->numeric()
+                            ->required()
+                            ->default(1),
+
+                        Forms\Components\TextInput::make('unit_price')
+                            ->label('سعر الوحدة')
+                            ->numeric()
+                            ->required()
+                            ->default(0),
+                    ]),
+            ])
+            ->actions([
+                EditAction::make()
+                    ->form([
+                        Forms\Components\Select::make('work_item_id')
+                            ->label('بند العمل')
+                            ->relationship('workItem', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Forms\Components\Select::make('unit_id')
+                            ->label('الوحدة')
+                            ->relationship('unit', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('quantity')
+                            ->label('الكمية')
+                            ->numeric()
+                            ->required(),
+
+                        Forms\Components\TextInput::make('unit_price')
+                            ->label('سعر الوحدة')
+                            ->numeric()
+                            ->required(),
+                    ]),
+
+                DeleteAction::make(),
             ]);
     }
 }
