f0948dfb313dadf2be5284ad51ae7c8ab2f6f10d
SHERIF\SHERIF <sherif.ganainy@gmail.com>
2025-12-23 08:17:22 +0200

    BOQ: add submit/back-to-draft actions on edit page

--- app/Filament/Resources/Boqs/Pages/EditBoq.php
+++ app/Filament/Resources/Boqs/Pages/EditBoq.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\Boqs\Pages;
 
 use App\Filament\Resources\Boqs\BoqResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditBoq extends EditRecord
@@ -13,6 +15,36 @@ class EditBoq extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('submit')
+                ->label('إرسال')
+                ->visible(fn () => (string) $this->record->status === 'DRAFT')
+                ->requiresConfirmation()
+                ->action(function () {
+                    $this->record->update(['status' => 'SUBMITTED']);
+
+                    Notification::make()
+                        ->title('تم إرسال المقايسة')
+                        ->success()
+                        ->send();
+
+                    $this->refreshFormData(['status']);
+                }),
+
+            Action::make('back_to_draft')
+                ->label('إرجاع لمسودة')
+                ->visible(fn () => (string) $this->record->status === 'SUBMITTED')
+                ->requiresConfirmation()
+                ->action(function () {
+                    $this->record->update(['status' => 'DRAFT']);
+
+                    Notification::make()
+                        ->title('تم إرجاع المقايسة لمسودة')
+                        ->success()
+                        ->send();
+
+                    $this->refreshFormData(['status']);
+                }),
+
             DeleteAction::make(),
         ];
     }
